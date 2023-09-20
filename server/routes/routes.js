d27cc34ca68a4a159f0a71e0b9a71f4e1e151773
Ankit Ydv <ankityadav11241@gmail.com>
2023-09-21 01:40:31 +0530

    add: all posts API

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -35,47 +35,80 @@ router.get("/users/:firebaseUserId", async (req, res) => {
 // followings add/minus
 router.put("/users/followings", async (req, res) => {
   const followId = req.body.followId;
-  const userDesc = await UserModel.findOne({ _id: req.body.userId });
-  console.log(userDesc);
-  if (userDesc.followings.includes(followId)) {
-    const userUnfollow = await UserModel.updateOne(
-      { _id: req.body.userId },
-      {
-        $pull: { followings: followId },
-      }
-    );
-    const userUnFollowSelf = await UserModel.updateOne(
-      { _id: followId },
-      {
-        $pull: { followers: req.body.userId },
-      }
-    );
-    res.send("unfollowed");
-  } else {
-    const userFollow = await UserModel.updateOne(
-      { _id: req.body.userId },
-      {
-        $push: { followings: followId },
-      }
-    );
-    const userFollowSelf = await UserModel.updateOne(
-      { _id: followId },
-      {
-        $push: { followers: req.body.userId },
-      }
-    );
-    res.send("following");
+  try {
+    const userDesc = await UserModel.findOne({
+      firebaseUserId: req.body.userId,
+    });
+    if (userDesc.followings.includes(followId)) {
+      const userUnfollow = await UserModel.updateOne(
+        { firebaseUserId: req.body.userId },
+        {
+          $pull: { followings: followId },
+        }
+      );
+      const userUnFollowSelf = await UserModel.updateOne(
+        { firebaseUserId: followId },
+        {
+          $pull: { followers: req.body.userId },
+        }
+      );
+      res.send("unfollowed");
+    } else {
+      const userFollow = await UserModel.updateOne(
+        { firebaseUserId: req.body.userId },
+        {
+          $push: { followings: followId },
+        }
+      );
+      const userFollowSelf = await UserModel.updateOne(
+        { firebaseUserId: followId },
+        {
+          $push: { followers: req.body.userId },
+        }
+      );
+      res.send("following");
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
 // --------------------------------------------------------------
 // Posts API
 
-router.get("/posts/:id", async (req, res) => {
-  console.log(req.body);
-  const user = await PostModel.find({ userId: req.body.userId });
-  console.log(user);
-  res.send(user);
+router.get("/posts/profileposts/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const posts = await PostModel.find({ userId: userId });
+    res.status(200).send({ "posts": posts });
+  } catch (err) {
+    res.status.apply(404).send({ error: "cannot get posts" });
+  }
+});
+
+router.get("/posts/all/:id", async (req, res) => {
+  const firebaseUserId = req.params.id;
+  let posts = [];
+  try {
+    // get user followings
+    const resp = await UserModel.findOne({ firebaseUserId: firebaseUserId });
+    if (resp) {
+      // Create an array of promises for fetching posts
+      const postPromises = resp.followings.map(async (id) => {
+        const post = await PostModel.find({ userId: id });
+        return post;
+      });
+
+      // Wait for all the promises to resolve
+      const allPosts = await Promise.all(postPromises);
+
+      // Flatten the array of arrays into a single posts array
+      posts = allPosts.flat();
+    }
+    res.status(200).send({ posts: posts });
+  } catch (err) {
+    res.status.apply(400).send({ error: "cannot get posts" });
+  }
 });
 
 router.post("/posts/new", async (req, res) => {
