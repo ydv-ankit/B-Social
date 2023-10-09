8de72a2f43129f92e97d1884314251095ec3b29e
Ankit Ydv <ankityadav11241@gmail.com>
2023-10-09 23:34:20 +0530

    fix: code breakdown

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,55 +1,37 @@
-import { useEffect } from 'react';
-import Newpost from '../Newpost/Newpost';
-import Posts from '../Posts/Posts';
-import { getStoredCookies } from '../../utils/cookies';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import Newpost from "../Newpost/Newpost";
+import Posts from "../Posts/Posts";
+import { getUserId } from "../../utils/cookies";
+import { useNavigate } from "react-router-dom";
 
-import './home.css'
+import "./home.css";
 
 export default function Home() {
+  const navigate = useNavigate();
+  let userData;
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    const userId = getUserId();
+    if (userId === null) {
+      navigate("/login");
+    }
+  }, [navigate]);
 
-
-    useEffect(() => {
-        const cookies = getStoredCookies()
-        const regex = /userId=([^',]+)/;
-
-        let userId = null;
-
-        // Loop through the array and extract userId values
-        cookies.forEach(item => {
-            const match = item.match(regex);
-            if (match) {
-                // Push the matched userId value into the array
-                userId = match[1];
-            }
-        });
-
-        if (userId === null) {
-            navigate("/login");
-        }
-    }, [navigate])
-
-    return (
-        <div className="home">
-            <div className="homeWrapper">
-                <div className="homeHead">
-                    <div className="homeHeadText">Home</div>
-                </div>
-                <div className="homeTopbar">
-                    <div className="homeForyou">
-                        For you
-                    </div>
-                    <div className="homeFollowing">
-                        Following
-                    </div>
-                </div>
-            </div>
-            <div className="homeNewPostContainer">
-                <Newpost />
-            </div>
-            <Posts />
+  return (
+    <div className="home">
+      <div className="homeWrapper">
+        <div className="homeHead">
+          <div className="homeHeadText">Home</div>
         </div>
-    )
+        <div className="homeTopbar">
+          <div className="homeForyou">For you</div>
+          <div className="homeFollowing">Following</div>
+        </div>
+      </div>
+      <div className="homeNewPostContainer">
+        <Newpost userData={userData} />
+      </div>
+      <Posts />
+    </div>
+  );
 }
