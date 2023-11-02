b4c4694de2b0fa3a457d8ed1889918a783d41114
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-02 23:54:27 +0530

    add: post preview

---
+++ client/src/components/PostPreview/PostPreview.jsx
@@ -0,0 +1,33 @@
+import "./postPreview.css"
+import PostSection from "../PostSection/PostSection"
+
+const PostPreview = ({ userPost }) => {
+    return (
+        <div className="postPreview">
+            <div className="postPreviewWrapper">
+                <div className="postPreviewTop">
+                    <div className="postPreviewUserFullname">
+                        <span>Ankit Ydv</span>
+                    </div>
+                    <div className="postPreviewUserPostTime">
+                        Post Time
+                    </div>
+                </div>
+                <div className="postPreviewContent">
+                    {/* <PostSection
+                        key={index}
+                        postId={element._id}
+                        postTime={time}
+                        data={userPostData}
+                        content={element.content}
+                        isLikedByUser={isLiked}
+                        commentCount={element.comments.length}
+                        likeCount={element.likes.length}
+                    /> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostPreview
