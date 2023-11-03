e71968302afda062ebb5fd23b566390c5cb6709f
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-03 16:46:29 +0530

    chore: method

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,38 +1,17 @@
 import PostSection from '../PostSection/PostSection';
 import { getUserId } from "../../utils/cookies";
+import { getPostTime } from '../../utils/getPostTime';
 import './posts.css';
 
 export default function Posts({ userPosts, userDetails }) {
-  const sortedPosts =userPosts && userPosts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  const sortedPosts = userPosts && userPosts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
     <>
       {sortedPosts && sortedPosts.length > 0 ? (
         sortedPosts.map((element, index) => {
-          const createdAt = element.createdAt; // Replace with your actual timestamp
-          const createdAtDate = new Date(createdAt);
-          const currentDateTime = new Date();
 
-          // Calculate the time difference in milliseconds
-          const timeDifference = currentDateTime - createdAtDate;
-
-          // Calculate hours from milliseconds
-          let time = Math.floor(timeDifference / 1000);
-          if (time > 60) {
-            // seconds to minutes
-            time = Math.floor(timeDifference / (1000 * 60)) + " s";
-          }
-          if (time > 60 * 60) {
-            // minutes to hours
-            time = Math.floor(timeDifference / (1000 * 60 * 60)) + " m";
-          }
-          if (time > 60 * 60 * 24) {
-            // hours to days
-            time = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) + " hrs";
-          } else {
-            // days to date
-            time = createdAtDate.toString().substring(4, 24);
-          }
+          const time = getPostTime(element.createdAt);
 
           let userId = getUserId();
           let isLiked = element.likes.includes(userId);
