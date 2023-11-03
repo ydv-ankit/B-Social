14c0e3e1189146eaa09526b2e859c2a5039bf4c7
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-03 17:32:25 +0530

    add: followers page

---
+++ client/src/components/followersPage/FollowersPage.jsx
@@ -0,0 +1,42 @@
+import './followersPage.css';
+import { useEffect, useState } from 'react';
+import UserList from '../userList/UserList';
+
+const FollowersPage = ({ userProfileId }) => {
+
+    const [userFollowersData, setUserFollowersData] = useState([]);
+    console.log(userFollowersData);
+
+    const getFollowers = async () => {
+        await fetch(process.env.REACT_APP_SERVER_URI + "users/get/followers/" + userProfileId)
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                setUserFollowersData(data.followersData);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    useEffect(() => {
+        getFollowers();
+    }, [])
+
+    return (
+        <div className="followers">
+            <div className="followersWrapper">
+                {
+                    userFollowersData && userFollowersData.length === 0
+                        ? <div className="nodata">No Followers</div>
+                        : userFollowersData && userFollowersData.map((element, _) => (
+                            <UserList key={_} data={element} />
+                        ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default FollowersPage
