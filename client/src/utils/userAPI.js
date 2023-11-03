3bc1b1d251e96bc58185a0ab0eed67d2fddc9e70
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-03 17:09:22 +0530

    add: user checks

--- client/src/utils/userAPI.js
+++ client/src/utils/userAPI.js
@@ -17,17 +17,27 @@ async function createUserDb(data) {
     lastSignInTime: lastSignInTime,
   };
   try {
-    const user = await fetch(process.env.REACT_APP_SERVER_URI + "users/new", {
-      method: "post",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    });
-    return true;
+    await fetch(process.env.REACT_APP_SERVER_URI + "users/" + email)
+      .then((resp) => {
+        return resp.json();
+      }).then(async (data) => {
+        if (data && data.status === "not found") {
+          const user = await fetch(process.env.REACT_APP_SERVER_URI + "users/new", {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+          });
+          return true;
+        }
+        else {
+          return false;
+        }
+      }).catch((err) => {
+        return false;
+      })
   } catch (error) {
-    console.log("error occured");
     return false;
   }
 }
