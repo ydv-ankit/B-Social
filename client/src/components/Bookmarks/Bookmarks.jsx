674476948bd6fef99d3d817a0e9c6e3803bc6939
Ankit Ydv <ankityadav11241@gmail.com>
2023-11-08 20:35:29 +0530

    bookmarks page

---
+++ client/src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import { getUserId } from "../../utils/cookies"
+import Posts from "../Posts/Posts";
+import Loader from "../loader/Loader";
+
+const Bookmarks = () => {
+  const [bookmarksData, setBookmarksData] = useState([]);
+  const [userDetails, setUserDetails] = useState([]);
+
+  const getBookmarks = async () => {
+    await fetch(process.env.REACT_APP_SERVER_URI + "post/get/bookmarks/" + getUserId())
+      .then((resp) => {
+        return resp.json()
+      }).then((data) => {
+        getUserDetails(data.data);
+        setBookmarksData(data.data);
+      })
+  }
+
+  const getUserDetails = async (data) => {
+    data.forEach(async (element) => {
+      await fetch(process.env.REACT_APP_SERVER_URI + "users/id/" + element.userId)
+        .then((resp) => {
+          return resp.json();
+        })
+        .then((response) => {
+          setUserDetails((prevData) => [...prevData, response.data])
+        }).catch((err) => {
+          console.log(err);
+        })
+    })
+  }
+
+  useEffect(() => {
+    getBookmarks();
+  }, [])
+
+  console.log(userDetails);
+
+  // if (userDetails.length === 0) {
+  //   return (
+  //     <Loader />
+  //   )
+  // }
+
+  return (
+    <div className="bookmarks">
+      {
+        bookmarksData.length > 0
+        ? <Posts userDetails={userDetails} userPosts={bookmarksData} />
+        : <div className="nodata">Go & save some bookmarks</div>
+      }
+    </div>
+  )
+}
+
+export default Bookmarks
